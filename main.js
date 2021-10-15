49c13fc4251e6775698f347b5ad1fd0be1e344c0
Ricardo Alves Roberto <ricbatera@bol.com.br>
2021-10-15 10:52:48 -0300

    card criado - compomentes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import './style.css'
+import './src/styles/settings/colors.css';
+import './src/styles/generic/reset.css'
+import './src/styles/elements/base.css'
+import cardGame from './src/components/CardGame';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const $root = document.querySelector('#root');
+const $htmlCardGame = cardGame();
+
+$root.insertAdjacentHTML('beforeend', $htmlCardGame);
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import "./style.css"
+
+function CardGame(){
+    return `
+    <article class="card-game">
+        <img src="src/img/alura-logo.png" alt="Logo Alura">
+    </article>    
+    `
+
+}
+
+export default CardGame;
