700054d6fc8261c65f343e05b3d6ba144c1eaa6b
Ricardo Alves <ricbatera@bol.com.br>
2021-10-16 16:03:49 -0300

    criado seta e implementado logica de virada de cartas

--- main.js
+++ main.js
@@ -3,19 +3,45 @@ import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 import BoardGame from './src/objects/BoardGame';
 import ScoreBoard from './src/objects/ScoreBoard';
+import SetaJogador from './src/components/SetaJogador';
 
 const $root = document.querySelector('#root');
 
 $root.insertAdjacentHTML('beforeend',`
+    ${SetaJogador()}
     ${ScoreBoard()}
     ${ BoardGame(6)}
 `);
 
 const $cards = document.querySelectorAll('.card-front-back');
+const $seta = document.getElementById('seta');
+let virados =0;
 $cards.forEach(e=>{
-    e.addEventListener('click',el=>{
-        e.classList.toggle('-ativo');
-        console.log(e)
+    e.addEventListener('click',()=>{
+        if(virados<2){
+            virados = contaCardsAtivos()
+            e.classList.toggle('-ativo');
+        }else{
+            desviraTodos()
+        }
     })
 })
+
+function desviraTodos(){
+    $cards.forEach(e =>{
+        e.classList.remove('-ativo')
+        virados = 0;        
+    })
+    $seta.classList.toggle('-posicao-player2')
+}
+
+function contaCardsAtivos(){
+    let ativos = 1;
+    $cards.forEach(e=>{
+        if(e.classList.contains('-ativo')){
+            ativos++
+        }
+    })
+    return ativos;
+}
 
---
+++ src/components/SetaJogador/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+function SetaJogador(){
+    return /*html*/ `
+    <div>
+        <img id="seta" class="posicao-player1" src='src/img/IconArrowDown.png' alt="Seta Jogador Selecionado">
+    </div>
+    `
+}
+export default SetaJogador;
