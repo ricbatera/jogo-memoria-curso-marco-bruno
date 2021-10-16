1e096ae7e5dd1ef7befedb9a2364974fec907779
Ricardo Alves <ricbatera@bol.com.br>
2021-10-16 10:10:55 -0300

    criado compomente player

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 import BoardGame from './src/objects/BoardGame';
+import PlayerName from './src/components/PlayerName';
 
 const $root = document.querySelector('#root');
 
-$root.insertAdjacentHTML('beforeend', BoardGame(8));
+$root.insertAdjacentHTML('beforeend',`
+    ${PlayerName('Player 1')}
+    ${PlayerName('Player 2')}
+    ${ BoardGame(8)}
+`);
 
 const $cards = document.querySelectorAll('.card');
 $cards.forEach(e=>{
---
+++ src/components/PlayerName/index.js
@@ -0,0 +1,4 @@
+function PlayerName(nome = 'Player X'){
+    return `<p> ${nome} </p>`
+}
+export default PlayerName;
