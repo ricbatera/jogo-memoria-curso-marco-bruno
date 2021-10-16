288af51eed88da5ab0f2bce602cb62a40f4f455f
Ricardo Alves <ricbatera@bol.com.br>
2021-10-16 13:48:12 -0300

    evento de click nos cartões

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 import BoardGame from './src/objects/BoardGame';
-import PlayerName from './src/components/PlayerName';
 import ScoreBoard from './src/objects/ScoreBoard';
 
 const $root = document.querySelector('#root');
 
 $root.insertAdjacentHTML('beforeend',`
     ${ScoreBoard()}
-    ${ BoardGame(2)}
+    ${ BoardGame(4)}
 `);
 
-const $cards = document.querySelectorAll('.card');
+const $cards = document.querySelectorAll('.card-front-back');
 $cards.forEach(e=>{
     e.addEventListener('click',el=>{
-        e.classList.toggle('esconde');
+        e.classList.toggle('-ativo');
         console.log(e)
     })
 })
