1c8aeaf9acfe626202dd8efe3a697f8f355ae6b5
Ricardo Alves <ricbatera@bol.com.br>
2021-10-16 09:41:22 -0300

    criado container dos cartoes e organizado

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 import './src/styles/settings/colors.css';
-import './src/styles/generic/reset.css'
-import './src/styles/elements/base.css'
-import CardGameCss from './src/components/CardGameCss';
-import CardGameHtml from './src/components/CardGameHtml';
-import CardGameJavaScript from './src/components/CardGameJavaScript';
+import './src/styles/generic/reset.css';
+import './src/styles/elements/base.css';
 import BoardGame from './src/objects/BoardGame';
 
 const $root = document.querySelector('#root');
 
-$root.insertAdjacentHTML('beforeend', BoardGame(9));
+$root.insertAdjacentHTML('beforeend', BoardGame(8));
 
 const $cards = document.querySelectorAll('.card');
 $cards.forEach(e=>{
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,6 +1,7 @@
+import "./styles.css"
 import CardGame from "../../components/CardGame";
 
 function BoardGame(qtde) {    
-    return CardGame().repeat(qtde);
+    return `<section class="board-game"> ${CardGame().repeat(qtde)}</section>`;
 }
 export default BoardGame;
